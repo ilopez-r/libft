cff63f428b7dca6eb316321b2470e0d1614b7390
ilopez-r <133966465+ilopez-r@users.noreply.github.com>
2023-05-19 13:29:59 +0200

    Update ft_split.c

--- ft_split.c
+++ ft_split.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft.h>
+#include "libft.h"
 
 int	ft_counter(char const *s, char c)
 {
@@ -31,4 +31,47 @@ int	ft_counter(char const *s, char c)
 	return (i);
 }
 
+void	ft_free(char **str, int i)
+{
+	while (i-- > 0)
+		free(str[i]);
+	free(str);
+}
+
+int	ft_len(char const *s, char c)
+{
+	int	len;
+
+	len = 0;
+	while (s[len] != '\0' && s[len] != c)
+		len++;
+	return (len);
+}
+
 char	**ft_split(char const *s, char c)
+{
+	char	**str;
+	int		i;
+	int		d;
+
+	d = 0;
+	i = 0;
+	str = malloc (ft_counter(s, c) + 1);
+	if (!str)
+		return (0);
+	while (i < ft_counter(s, c))
+	{
+		while (s[d] == c)
+			d++;
+		str[i] = ft_substr(s, d, ft_len(s, c));
+		if (!(str[i]))
+		{
+			ft_free(str, i);
+			return (0);
+		}
+		d = d + ft_len(s, c);
+		i++;
+	}
+	str[i] = 0;
+	return (str);
+}
